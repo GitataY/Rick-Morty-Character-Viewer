2bb596d176f5beb0b43b58f3b48f7e9430a1c626
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2024-04-16 15:47:48 +0300

    pagination

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
+import Pagination from './components/Pagination/Pagination';
 
 
 function App() {
@@ -42,6 +43,8 @@ function App() {
           </div>
         </div>
       </div>
+
+      <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber}/>
 
       
 
