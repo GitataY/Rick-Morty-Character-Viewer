c307e2b73a22dc544c5e9077e16935477e0de068
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2024-04-17 10:47:30 +0300

    update filter

--- src/components/Filters/FilterBTN.js
+++ src/components/Filters/FilterBTN.js
@@ -1,35 +1,38 @@
-import React from 'react'
+import React from 'react';
 
-const FilterBTN = ({name, index, items, task, setPageNumber}) => {
+const FilterBTN = ({ name, index, items, task, setPageNumber }) => {
+  const radioStyle = {
+    display: 'none', // hides the radio button
+  };
+
+  const labelStyle = {
+    cursor: 'pointer', // adds pointer cursor on hover
+    // Add additional styling as needed
+  };
+
   return (
     <div>
-        <style jsx>
-            {
-                `
-                .x:checked + label{
-                    background-color : #0b5ed7;
-                    color: white;
-                }
-                input[type='radio']{
-                    display:none
-                }`
-            }
-
-        </style>
-
-        <div className="form-check">
-            <input 
-            onClick={() => {
-                setPageNumber(1);
-                task(items)
-            }}
-            className="form-check-input x" type="radio" name="{name}" id={`${name}-${index}`} />
-            <label class="btn btn-outline-primary" for={`${name}-${index}`}>
-                {items}
-            </label>
-        </div>
+      <div className="form-check">
+        <input 
+          onClick={() => {
+            setPageNumber(1);
+            task(items);
+          }}
+          style={radioStyle}
+          className="form-check-input" 
+          type="radio" 
+          name={name} 
+          id={`${name}-${index}`} 
+        />
+        <label 
+          style={labelStyle}
+          className="btn btn-outline-primary" 
+          htmlFor={`${name}-${index}`}>
+          {items}
+        </label>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FilterBTN
+export default FilterBTN;
